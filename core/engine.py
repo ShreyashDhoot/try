782365f5d6eb7e5fcad62c07dab4283352e17820
ShreyashDhoot <shreyashdhoot@gmail.com>
2026-02-13 16:54:51 +0530

    first commit

--- core/engine.py
+++ core/engine.py
@@ -22,6 +22,7 @@ class DiffusionEngine:
         # 3. Diffusion Loop
         for i, t in enumerate(tqdm(self.pipe.scheduler.timesteps, desc="Generating")):
             # Standard Denoising Step
+            print(f"denoising for step {i}")
             latent_model_input = torch.cat([latents] * 2)
             latent_model_input = self.pipe.scheduler.scale_model_input(latent_model_input, t)
             
@@ -30,25 +31,21 @@ class DiffusionEngine:
             
             noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
             noise_pred = noise_pred_uncond + config.GUIDANCE_SCALE * (noise_pred_text - noise_pred_uncond)
-            latents = self.pipe.scheduler.step(noise_pred, t, latents).prev_sample
+            latents = self.pipe.scheduler.step(noise_pred, t, latents).prev_sample 
+            current_pil = decode_to_pil(self.pipe, latents)
+            score, binary_mask = auditor.audit(current_pil)
 
-            # 4. Audit & Inpaint (Selective Triggering)
-            # You can audit every step or every N steps (e.g., if i % 5 == 0)
-            if i % 1 == 0: 
-                current_pil = decode_to_pil(self.pipe, latents)
-                score, binary_mask = auditor.audit(current_pil)
-
-                if score > config.GLOBAL_THRESHOLD:
-                    print(f"| Step {i} | Adversary Detected (Score: {score:.2f}) | Deploying Surgeon...")
+            if score > config.GLOBAL_THRESHOLD:
+                print(f"| Step {i} | Adversary Detected (Score: {score:.2f}) | Deploying Surgeon...")
                     
-                    # Call the modular inpainter logic
-                    # This handles the internal pixel-fix and the latent graft
-                    latents, _ = surgeon.run(
-                        prompt=safe_target,
-                        current_pil=current_pil,
-                        binary_mask=binary_mask,
-                        t=t,
-                        latents=latents
-                    )
+                # Call the modular inpainter logic
+                # This handles the internal pixel-fix and the latent graft
+                latents, _ = surgeon.run(
+                    prompt=safe_target,
+                    current_pil=current_pil,
+                    binary_mask=binary_mask,
+                    t=t,
+                    latents=latents
+                )
         
         return decode_to_pil(self.pipe, latents)
